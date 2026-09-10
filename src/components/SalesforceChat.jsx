c6afa590f6e0fbedb0a4301b3d9806d32ad5b755
Samarth Kapadia <samarthkapadia@Samarths-MacBook-Air.local>
2026-09-10 16:37:07 +0530

    updated salesforce chatagent embeddment

--- src/components/SalesforceChat.jsx
+++ src/components/SalesforceChat.jsx
@@ -1,32 +1,27 @@
-// SalesforceChat.jsx
 import { useEffect } from "react";
 
 export default function SalesforceChat() {
   useEffect(() => {
-    // Avoid loading the script multiple times if it already exists
-    const scriptId = "salesforce-embedded-messaging";
-    if (document.getElementById(scriptId)) return;
-
     const script = document.createElement("script");
-    script.id = scriptId;
-    script.type = "text/javascript";
+
     script.src =
-      "https://cloudage.my.site.com/ESWCloudAgeWebChat1788951009790/assets/js/bootstrap.min.js";
+      "https://cloudage.my.site.com/ESWCloudAgeAgentDeploym1789037913097/assets/js/bootstrap.min.js";
 
     script.onload = () => {
       try {
         window.embeddedservice_bootstrap.settings.language = "en_US";
 
         window.embeddedservice_bootstrap.init(
-          "00Daj000016dxyv",
-          "CloudAge_Web_Chat",
-          "https://cloudage.my.site.com/ESWCloudAgeWebChat1788951009790",
+          "00DgK00000Q8rue",
+          "CloudAge_Agent",
+          "https://cloudage.my.site.com/ESWCloudAgeAgentDeploym1789037913097",
           {
-            scrt2URL: "https://cloudage.my.salesforce-scrt.com",
+            scrt2URL:
+              "https://cloudage.my.salesforce-scrt.com",
           }
         );
       } catch (err) {
-        console.error("Error loading Embedded Messaging: ", err);
+        console.error(err);
       }
     };
 
