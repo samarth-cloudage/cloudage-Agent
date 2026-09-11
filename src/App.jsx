86f6c0dd97a52b519c2347c00e21d18b25f21e83
Samarth Kapadia <samarthkapadia@Samarths-MacBook-Air.local>
2026-09-11 14:17:21 +0530

    testing prompt suggestion again

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import GlowCursor from "./components/GlowCursor";
 import ScrollToTop from "./components/ScrollToTop";
 import SalesforceChat from "./components/SalesforceChat";
 import ScrollToTopButton from "./components/ScrollToTopButton";
-import ChatStarterPrompts from "./components/ChatStarterPrompts";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
@@ -28,10 +28,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <>
-  <ChatStarterPrompts />
-  <SalesforceChat />
-</>
+        <SalesforceChat />
       <ScrollToTop />
       <CursorFollower />
       <GlowCursor />
